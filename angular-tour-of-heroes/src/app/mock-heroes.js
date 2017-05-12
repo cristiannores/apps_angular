7ea56aa616a655dd4bcc00d31b6c39674e2c8e92
cristian nores <cristian.nores@gmail.com>
2017-05-12 16:38:18 -0300

    inicial

---
+++ angular-tour-of-heroes/src/app/mock-heroes.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.HEROES = [
+    { id: 2, name: "wolverine" },
+    { id: 3, name: "superman" },
+    { id: 4, name: "mystic" },
+    { id: 5, name: "rogue" },
+    { id: 6, name: "DR. X" },
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+  {id:2, name : "wolverine"},
+  {id:3, name : "superman"},
+  {id:4, name : "mystic"},
+  {id:5, name : "rogue"},
+  {id:6, name : "DR. X"},
+];
